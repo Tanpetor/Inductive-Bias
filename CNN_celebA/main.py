35f5d375066ba53a03fd0665353db12cf9ddfebe
Xipeng Tan <stan_1@edu.hse.ru>
2025-04-14 18:39:26 +0300

    Basic model

--- CNN_celebA/main.py
+++ CNN_celebA/main.py
@@ -2,12 +2,14 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from CNN_IB_model import InductiveBiasCelebA
+from CNN_model import CelebA15ClassClassifier
 from data_utils import prepare_dataloaders, dataset
 from Train import train_15class_model
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'mps')
 print(f'Using device: {device}')
 
+#
 model = InductiveBiasCelebA(n_classes=15)
 criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
 optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
